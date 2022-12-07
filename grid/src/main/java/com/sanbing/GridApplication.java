21ab8bc632eceb98d1f10c3d366558c1dae10e8e
sanbing <zlck20141341021@163.com>
2022-12-07 17:04:21 +0800

    1.创建数字货币相关功能的项目（父子模块）

---
+++ grid/src/main/java/com/sanbing/GridApplication.java
@@ -0,0 +1,15 @@
+package com.sanbing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class GridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridApplication.class, args);
+	}
+
+}
